1b98e28ba2f8e37f8cb5bba540cf708d0891b958
Sibongile <sibongile.nhlema@gmail.com>
2023-10-08 17:58:50 +0000

    Added new solution to code. Does not fork if command does not exist

--- execute_command.c
+++ execute_command.c
@@ -1,6 +1,8 @@
 #include "shell.h"
 
-void search_and_execute_command(char **tokens);
+int search_for_command(char **tokens);
+char *search_in_dir(char **tokens);
+char *exe_in_dir(char **tokens);
 
 /**
  * execute_command - executes command line agruments and handles path
@@ -11,84 +13,144 @@ void execute_command(char **tokens)
 {
 	pid_t pid;
 
-	pid = fork();
+	if (search_for_command(tokens) == 0)
+	{
+		pid = fork();
 
-	if ((pid) == -1)
-	{
-		perror("Error: Failed to fork the current process.\n");
-		exit(EXIT_FAILURE);
-	}
-	if (pid == 0)
-	{
-		search_and_execute_command(tokens);
+		if ((pid) == -1)
+		{
+			perror("Error: Failed to fork the current process.\n");
+			exit(EXIT_FAILURE);
+		}
+		if (pid == 0)
+		{
+			if ((execve(tokens[0], tokens, environ) == -1))
+			{
+				exe_in_dir(tokens);
+			}
+		}
+		else
+		{
+			wait(NULL);
+		}
 	}
 	else
 	{
-		wait(NULL);
+		printf("Error: '%s' command not found.\n", tokens[0]);
 	}
 }
 
 /**
- * search_and_execute_command - finds command or path and executes accordingly
+ * search_for_command - finds command in current directory
  * @tokens: command and arguments
+ *
+ * Return: 1 on error, 0 on success
  */
 
-void search_and_execute_command(char **tokens)
+int search_for_command(char **tokens)
+{
+	if (myCustomStrchr(tokens[0], '/') != NULL) /* Check if cmd has path*/
+	{
+		return (0);
+	}
+	else
+	{
+		if (search_in_dir(tokens) != NULL)
+			return (0);
+		else
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * search_in_dir - searches in directories for full path
+ * @tokens: command and arguments entered by user
+ *
+ * Return: Null if not found, commandPath if found in dir
+ */
+
+char *search_in_dir(char **tokens)
 {
 	char *path, *commandPath, *pathCopy, *token = NULL;
 
-	/* Check if command contains a path*/
-	if (myCustomStrchr(tokens[0], '/') != NULL)
+	path = myCustomGetenv("PATH");
+	pathCopy = myCustomStrdup(path);
+	token = strtok(pathCopy, ":");
+
+	if (pathCopy == NULL)
 	{
-		if (execve(tokens[0], tokens, NULL) == -1)
-		{
-			perror("Error: No such file or directory.\n");
-			exit(EXIT_FAILURE);
-		}
+		perror("Error: Failed to allocate memory for pathCopy.\n");
+		exit(EXIT_FAILURE);
 	}
-	else /* Search for command in directories specified by PATH env var */
+	while (token != NULL)
 	{
-		path = myCustomGetenv("PATH");
-		pathCopy = myCustomStrdup(path);
-		token = strtok(pathCopy, ":"); /* Split PATH str into individual dir */
+		commandPath = malloc(myCustomStrlen(token)
+				+ myCustomStrlen(tokens[0]) + 2);
+		myCustomStrcpy(commandPath, token);
+		myCustomStrcat(commandPath, "/"); /* Append a slash to directory path */
+		myCustomStrcat(commandPath, tokens[0]); /* Append command name to dir path */
 
-		if (pathCopy == NULL)
+		if (commandPath == NULL)
 		{
-			perror("Error: Failed to allocate memory for pathCopy.\n");
+			perror("Error: Failed to allocate memory for commandPath.\n");
 			exit(EXIT_FAILURE);
 		}
-
-		while (token != NULL)
+		if (access(commandPath, X_OK) == 0)
 		{
-			commandPath = malloc(myCustomStrlen(token) + myCustomStrlen(tokens[0]) + 2);
-			myCustomStrcpy(commandPath, token);
-			myCustomStrcat(commandPath, "/"); /* Append a slash to directory path */
-			/* Append command name to directory path */
-			myCustomStrcat(commandPath, tokens[0]);
+			return (commandPath);
+		}
+		free(commandPath);
+		token = strtok(NULL, ":"); /* Move to next directory in PATH */
+	}
+	free(pathCopy);
+	return (NULL);
+}
 
-			if (commandPath == NULL)
+/**
+ * exe_in_dir - executes command found in path
+ * @tokens: command and argument
+ *
+ * Return: Null on success, nothing on error
+ */
+
+char *exe_in_dir(char **tokens)
+{
+	char *path, *commandPath, *pathCopy, *token = NULL;
+
+	path = myCustomGetenv("PATH");
+	pathCopy = myCustomStrdup(path);
+	token = strtok(pathCopy, ":");
+
+	if (pathCopy == NULL)
+	{
+		perror("Error: Failed to allocate memory for pathCopy.\n");
+		exit(EXIT_FAILURE);
+	}
+	while (token != NULL)
+	{
+		commandPath = malloc(myCustomStrlen(token)
+				+ myCustomStrlen(tokens[0]) + 2);
+		myCustomStrcpy(commandPath, token);
+		myCustomStrcat(commandPath, "/");
+		myCustomStrcat(commandPath, tokens[0]);
+
+		if (commandPath == NULL)
+		{
+			perror("Error: Failed to allocate memory for commandPath.\n");
+			exit(EXIT_FAILURE);
+		}
+		if (access(commandPath, X_OK) == 0)
+		{
+			if ((execve(commandPath, tokens, environ) == -1))
 			{
-				perror("Error: Failed to allocate memory for commandPath.\n");
+				perror("Error: No such file or directory.\n");
 				exit(EXIT_FAILURE);
 			}
-			if (access(commandPath, X_OK) == 0)
-			{
-				if (execve(commandPath, tokens, environ) == -1) /* Execute the command */
-				{
-					perror("Error: Failed to execute the command.\n");
-					exit(EXIT_FAILURE);
-				}
-				else
-				{
-					perror("Error: No such file or directory.\n");
-					exit(EXIT_FAILURE);
-				}
-			}
-			free(commandPath);
-			token = strtok(NULL, ":"); /* Move to next directory in PATH */
 		}
-		free(pathCopy);
+		free(commandPath);
+		token = strtok(NULL, ":");
 	}
-	printf("Error: '%s' is not a valid command.\n", tokens[0]);
-	exit(EXIT_FAILURE);
+	free(pathCopy);
+	return (NULL);
 }
