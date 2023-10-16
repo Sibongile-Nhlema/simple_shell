86ea3ab437a72f9bc211a84f60883145303ac585
Sibongile <sibongile.nhlema@gmail.com>
2023-10-16 20:02:21 +0000

    betty checked setenv.c

--- setenv.c
+++ setenv.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * myCsutomSetenv - change or adds an environment variable
+ * myCustomSetenv - change or adds an environment variable
  * @name: name of variable
  * @value: the value to use
  * @overwrite: an interger
@@ -16,52 +16,39 @@ int myCustomSetenv(const char *name, const char *value, int overwrite)
 	int index, environ_size = 0;
 	(void)overwrite;
 
-	/*find length of new env var*/
-	if (name == NULL || value == NULL)
+	if (name == NULL || value == NULL)/*find length of new env var*/
 		return (-1); /*avoid segmentation fault*/
-
 	len = const_strlen(name) + const_strlen(value) + 2;
 	new_var = (char *)malloc(len);/*memory for new env var*/
 	if (new_var == NULL)
 		return (-1);
-	const_strcpy(new_var, name);
-	const_strcat(new_var, "=");
+	const_strcpy(new_var, name), const_strcat(new_var, "=");
 	const_strcat(new_var, value);
-
 	while (environ[environ_size] != NULL)
 		environ_size++;
-
 	old_environ = (char **)malloc((environ_size + 1) * sizeof(char *));
 	memcpy(old_environ, environ, (environ_size + 1) * sizeof(char *));
-
 	index = 0; /*find position of env var in environ array*/
 	while (old_environ[index] != NULL)
 	{
 		if (const_strncmp(old_environ[index], name, const_strlen(name)) == 0)
 		{
-			old_environ[index] = new_var;
-			environ = old_environ;
+			old_environ[index] = new_var, environ = old_environ;
 			return (0);
 		}
 		else/*got rid of memory leak errors*/
 		{
-			free(new_var);
-			free(old_environ);
+			free(new_var), free(old_environ);
 			return (0);
 		}
-		index++;
-	}
-	new_environ = (char **)realloc(old_environ,(index + 2) * sizeof(char *));
+		index++; }
+	new_environ = (char **)realloc(old_environ, (index + 2) * sizeof(char *));
 	if (new_environ == NULL)
 	{
-		free(old_environ);
-		free(new_var);
-		return (-1);
-	}
-	old_environ = new_environ;
-	free(old_environ);
-	new_environ[index] = new_var;
-	new_environ[index + 1] = NULL;
+		free(old_environ), free(new_var);
+		return (-1); }
+	old_environ = new_environ, free(old_environ);
+	new_environ[index] = new_var, new_environ[index + 1] = NULL;
 	environ = new_environ;
 	return (0);
 }
